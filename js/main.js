e7ecc8d33dfb2bdafffd0c20a7fad92d07b0af69
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-27 14:09:39 +0900

    MOBILE

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// VISUAL영역 슬라이드
 new Swiper('#visual .swiper-container', {
   loop: true,
   autoplay: true,
@@ -12,13 +13,46 @@ new Swiper('#visual .swiper-container', {
 });
 
 
+// MOVIES 슬라이드
 new Swiper('#movies .swiper-container', {
   loop: true,
   autoplay: {
     delay: 5000
   },
   slidesPerView: 4,
-  spaceBetween: 24
+  spaceBetween: 24,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 10
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    960: {
+      slidesPerView: 4,
+      spaceBetween: 24
+    },
+    // 1200: {
+    //   slidesPerView: 4,
+    //   spaceBetween: 24,
+    // }
+  },
+      mousewheel: {
+        invert: true,
+  },
 });
 
 
@@ -30,3 +64,4 @@ noticeBtns.forEach(function (noticeBtn) {
     noticeBtn.classList.toggle('active');
   })
 })
+
