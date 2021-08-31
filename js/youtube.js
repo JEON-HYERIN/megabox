c8463aabfd4e8c59eac75e433bd8d5984d6e5230
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-08-31 13:10:26 +0900

    last touch_0831

---
+++ js/youtube.js
@@ -0,0 +1,26 @@
+// Youtube IFrame API를 비동기로 로드합니다.
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubePlayerAPIReady() {
+  // <div id="player"></div>
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 재생할 유튜브 영상 ID
+    height: 500,
+    // width: 816,
+    playerVars: {
+      controls: 0,
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: 'l8BpJ8vBDZI' // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      // 영상이 준비되었을 때,
+      onReady: function (event) {
+        event.target.mute(); // 음소거!
+      }
+    }
+  });
+}
